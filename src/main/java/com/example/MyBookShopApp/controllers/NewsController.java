a774d527a15ea54d7d15e08f078506271234b155
aleksandr_krasnikov_1 <aa_krasnikov@mail.ru>
2023-07-09 12:42:41 +0400

    Добавлен вывод книг на странице Новинок и настроены хлебные крошки

--- src/main/java/com/example/MyBookShopApp/controllers/NewsController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/NewsController.java
@@ -1,22 +1,28 @@
 package com.example.MyBookShopApp.controllers;
+import com.example.MyBookShopApp.data.Book;
+import com.example.MyBookShopApp.service.BookService;
 import com.example.MyBookShopApp.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.List;
+
 @Controller
 public class NewsController {
     private final NewsService newsService;
+    private final BookService bookService;
 
     @Autowired
-    public NewsController(NewsService newsService) {
+    public NewsController(NewsService newsService, BookService bookService) {
         this.newsService = newsService;
+        this.bookService =bookService;
     }
 
     @ModelAttribute("newsBooks")
-    public void newsBooks () {
-
+    public List<Book> newsBooks () {
+        return bookService.getBooksNew();
     }
 
     @GetMapping("/books/recent")
